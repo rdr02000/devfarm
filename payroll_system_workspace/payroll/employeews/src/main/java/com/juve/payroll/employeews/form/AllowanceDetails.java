322acd7a3d48f7626b1b8371500a6b77f168c5bb
Ramel C. Rosales <rosalesramel@gmail.com>
2017-07-15 16:00:46 +0800

    remove comment

--- payroll_system_workspace/payroll/employeews/src/main/java/com/juve/payroll/employeews/form/AllowanceDetails.java
+++ payroll_system_workspace/payroll/employeews/src/main/java/com/juve/payroll/employeews/form/AllowanceDetails.java
@@ -3,26 +3,15 @@ package com.juve.payroll.employeews.form;
 import java.util.List;
 
 public class AllowanceDetails {
-<<<<<<< HEAD
 	private long employeeId;
-=======
-	private String employeeId;
->>>>>>> 52fca8eec3635ab310370236e1c80aca7322d456
 	private String firstName;
 	private String lastName;
 	private List<Allowance> allowanceList;
 	
-<<<<<<< HEAD
 	public long getEmployeeId() {
 		return employeeId;
 	}
 	public void setEmployeeId(long employeeId) {
-=======
-	public String getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(String employeeId) {
->>>>>>> 52fca8eec3635ab310370236e1c80aca7322d456
 		this.employeeId = employeeId;
 	}
 	public String getFirstName() {
